fe0a85f87f48555578ad89d947dc0266534ad12c
unitedcctv <karl@dlab.io>
2025-01-30 09:54:16 +0100

    add custom marker image

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css'
+import L from 'leaflet'
 import './App.css'
+import markerIcon from './theme/marker.png'
 
-const position = [52.4967277, 13.4401145]
+const position = [52.4969000, 13.4403000]
+
+const customIcon = L.icon({
+  iconUrl: markerIcon,
+  iconSize: [100, 50],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34]
+})
 
 function App() {
   return (
@@ -13,13 +22,13 @@ function App() {
         <MapContainer 
           center={position} 
           zoom={13} 
-          style={{ height: '400px', width: '400px' }}
+          style={{ height: '800px', width: '800px' }}
         >
           <TileLayer
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           />
-          <Marker position={position}>
+          <Marker position={position} icon={customIcon}>
             <Popup>
               dlab location
             </Popup>
